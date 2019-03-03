9f0aadf3f5eca5a02bf454cda416a7c7c85f737e
aliyarahman <aliya.rahman@yahoo.com>
2019-03-03 14:34:45 -0500

    Completed console tictactoe except for AI logic on computer moves

--- oneD/tictactoe/tictactoe/tictactoe.cpp
+++ oneD/tictactoe/tictactoe/tictactoe.cpp
@@ -1,3 +1,7 @@
+// ########################################
+// # TicTacToe 1.0 (against the computer) #
+// ########################################
+
 #include "pch.h"
 #include <iostream>
 #include <string>
@@ -5,35 +9,149 @@
 #include <algorithm>
 using namespace std;
 
+const char X = 'X';
+const char O = 'O';
+const char EMPTY = ' ';
+const char TIE = 'T';
+const char NO_ONE = 'N';
+
+void instructions(); // Displays game instructions
+char askYesNo(string question); // asks a yes/no question and receives a y or n char
+int askNumber(string question, int high, int low = 0); // Asks for a number between low and high
+char humanPiece(); // Determines if it is a human piece, yeterns 'X' or 'O'
+char opponent(char piece); // Calculates opposing piece (X/O), given a piece
+void displayBoard(const vector<char>& board); // Receives a board array and displays it on screen
+char winner(const vector<char>& board); // Determines the game winner (X, O, Tie, None)
+bool isLegal(const vector<char>& board, int move); // Determines whether a move is legal
+int humanMove(const vector<char>& board, char human); // Get's the human's move
+int computerMove(vector<char> board, char computer); // Calculates the computer's move
+void announceWinner(char winner, char computer, char human); // Declares winner or tie
+
+
 int main()
 {
-	char rawResponse, firstTurn, playAgain;
-	bool keepPlaying = true;
-	//int squareNum;
+	int move;
+	const int NUM_SQUARES = 9;
+	vector<char> board(NUM_SQUARES, EMPTY);
 
-	while (keepPlaying == true) {
-		cout << "Welcome to 1-D TicTacToe\n\n";
-		
-		cout << "\n 1 | 2 | 3 \n";
-		cout << "-----------\n";
-		cout << " 4 | 5 | 6 \n";
-		cout << "-----------\n";
-		cout << " 7 | 8 | 9 \n";
-		
-		cout << "\n Do you want the first turn? (Y)es  (N)o: ";
-		cin >> rawResponse;
-		firstTurn = tolower(rawResponse);
-		
-		while (0) {
-			// Game
+	instructions();
+	char human = humanPiece();
+	char computer = opponent(human);
+	char turn = X;
+	displayBoard(board);
+
+	while (winner(board) == NO_ONE) {
+		if (turn == human) {
+			move = humanMove(board, human);
+			board[move] = human;
 		}
-		
-		cout << "\n Play again? (Y)es  (N)o: ";
-		cin >> rawResponse;
-		playAgain = tolower(rawResponse);
-		if (playAgain == 'n') { keepPlaying = false;}
+		else {
+			move = computerMove(board, computer);
+			board[move] = computer;
+		}
+		displayBoard(board);
+		turn = opponent(turn);
 	}
-	cout << "\n\nGoodbye!\n\n" << endl;
-	cout << endl;
+
+	announceWinner(winner(board), computer, human);
+
 	return 0;
+}
+
+
+void instructions() {
+	cout << "\nWelcome to Tic Tac Toe!\n";
+}
+
+char askYesNo(string question) {
+	char rawResponse;
+	do {
+		cout << "\n" + question + "? (y)es  (n)o: ";
+		cin >> rawResponse;
+	} while ((rawResponse != 'y') && (rawResponse != 'n'));
+	return rawResponse;
+}
+
+
+int askNumber(string question, int high, int low) {
+	int rawResponse;
+
+	do {
+		cout << "\n" << question << low << " - " << high << ": ";
+		cin >> rawResponse;
+	} while ((rawResponse < low) && (rawResponse > high));
+
+	return rawResponse;
+}
+
+
+char humanPiece() {
+	char human = askYesNo("Player X goes first. Do you want to be player X?");
+	if (human == 'y') {
+		return 'X';
+	}
+	else {
+		return 'O';
+	}
+}
+
+char opponent(char piece) {
+	char oppo;
+	oppo = (piece == 'X' ? 'O' : 'X');
+	return oppo;
+}
+
+void displayBoard(const vector<char>& board) {
+	cout << "Welcome to 1-D TicTacToe\n\n";
+
+	cout << "\n " << board[0] << " | " << board[1] << " | " << board[2];
+	cout << "-----------";
+	cout << " " + board[3] << " | " << board[4] << " | " << board[5];
+	cout << "-----------";
+	cout << " " + board[6] << " | " << board[7] << " | " << board[8];
+	cout << "\n\n";
+}
+
+char winner(const vector<char>& board) {
+	const int TOTAL_ROWS = 8;
+	const int WINNING_ROWS[8][3] = { { 0,1,2 }, { 3,4,5 }, { 6,7,8 }, { 0,3,6 }, { 1,4,7 }, { 2,5,8 } };
+	
+	for (int i = 0; i < TOTAL_ROWS; ++i) {
+		if ((board[WINNING_ROWS[i][0]] != EMPTY) && 
+			((board[WINNING_ROWS[i][0]] == board[WINNING_ROWS[i][2]]) 
+				&& (board[WINNING_ROWS[i][1]] == board[WINNING_ROWS[i][2]]))) {
+			return board[WINNING_ROWS[i][0]];
+		}
+		if (count(board.begin(), board.end(), EMPTY) == 0) {
+			return TIE;
+		}
+	}
+	return NO_ONE;
+}
+
+bool isLegal(int move, const vector<char> board) {
+	return (board[move] == EMPTY);
+ }
+
+int humanMove(const vector<char>& board, char human) {
+	int response = askNumber("Where will you move?", (board.size() - 1));
+
+	while (!isLegal(response, board)) {
+		cout << "\nThat square is taken - choose again!\n" ;
+		response = askNumber("Where will you move?", (board.size() - 1));
+	}
+	return response;
+}
+
+int computerMove(const vector<char> board, char computer) {
+	//
+}
+
+void announceWinner(char winner, char computer, char human) {
+	if (winner == computer || winner==human) {
+		cout << winner << "wins!\n";
+	}
+	else {
+		cout << "It's a tie!\n";
+	}
 }
